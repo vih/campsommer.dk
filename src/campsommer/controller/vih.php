e1ed073ba046160281d0349aab66e5f4469852e4
Lars Olesen <lars@intraface.dk>
2008-01-22 19:42:22 +0000

    Updated text to the new one.

--- src/campsommer/controller/vih.php
+++ src/campsommer/controller/vih.php
@@ -15,28 +15,30 @@ class Campsommer_Controller_Vih extends k_Controller
     {
             return '
 <h1>SommerCamp Vejle, uge 27, 28 og 29, 2008</h1>
-<p><strong>3 ugers sommerhøjskole for unge, der vil forbedre deres skolekundskaber og dyrke en masse idræt.</strong></p>
-<p>Mangler du færdigheder i almindelige skolefag, som dansk, matematik, samfundsfag og historie, er dette kurset for dig. Hvis du samtidig er interesseret i idræt, eller bare vil i bedre fysisk form er der alle muligheder for at opleve 3 uforglemmelige uger sammen med andre unge. </p>
-<h2>Beskrivelse af fagområderne:</h2>
+<h2>- Tre uforglemmelige uger</h2>
+
 <ul>
-<li>Dansk: Du skal arbejde med stavning og grammatik. Gennem øvelser og opgaver bliver din evne til at kommunikere både skriftligt og mundtligt forbedret.</li>
-<li>Matematik: Der fokuseres på almindelig færdighedsregning, procent- og brøkregning. Du vil møde en række grundlæggende matematiske principper, som vil give dig en større indsigt i matematik.</li>
-<li>Samfundsfag: Der gives en oversigt over institutioner i Danmark og Europa, samt betydningen af disse for borgerne. Demokratiet i DK og dets virkeform gennemgås. Du får en generel indføring i almen viden, som svarer til kravene til politiets optagelsesprøve.</li>
-<li>Kommunikation og teambuilding: Du kommer til at stå i situationer, hvor du skal lave aktiviteter for de andre elever, og hvor I skal løse en opgave sammen.</li>
+    <li>Dansk</li>
+    <li>Matematik</li>
+    <li>Samfundsfag</li>
+    <li>Kommunikation</li>
+    <li>Teambuilding</li>
+    <li>Idræt</li>
 </ul>
-<p>Den idrætsfaglige del af kurset vil primært bestå af boldspil, fitness, svømning og vandaktiviter. Du kan vælge mellem forskellige muligheder efter din interesse. Der bliver i modsætning til de almindelige skolefag mulighed for afprøvning af mange forskellige idrætsgrene.</p>
 
-<p>Du får mulighed for at få vejledning i forbindelse med valg af uddannelse, eller bare en snak om den uddannelse du skal i gang med eller er i gang med. Dette foregår dels i grupper med de øvrige elever, dels med en af skolens lærere.<br>
-Du skulle gerne efter 3 uger på Vejle Idrætshøjskole have dannet dig et overblik over muligheder/konsekvenser af dine ønsker/planer. </p>
-<h2>Modelskema for kurset:</h2>
-<p><strong>Mandag:</strong> Morgensamling, dansk, idræt, matematik, vejledning, forberedelse, personlige samtaler/frivillig idræt.</p>
-<p><strong>Tirsdag:</strong> Morgensamling, samfundsfag, idræt, dansk, forberedelse, film.</p>
-<p><strong>Onsdag:</strong> Morgensamling, matematik, foredrag, idræt, forberedelse, personlige samtaler/frivillig idræt</p>
+<p><strong>3 ugers sommercamp for dig mellem 17½ og 30 år, der gerne vil forbedre dine skolekundskaber og dyrke en masse idræt. Det er tre uforglemmelige uger som giver dig nye muligheder i livet, og hvor du møder en masse nye venner.</strong></p>
+<ul>
+<li><strong>Dansk</strong>: Du skal arbejde med stavning og grammatik. Gennem øvelser og opgaver bliver din evne til at kommunikere både skriftligt og mundtligt forbedret.</li>
+<li><strong>Matematik</strong>: Der fokuseres på almindelig færdighedsregning, procent- og brøkregning. Du vil møde en række grundlæggende matematiske principper, som vil give dig en større indsigt i matematik.</li>
+<li><strong>Samfundsfag</strong>: Der gives en oversigt over institutioner i Danmark og Europa, samt betydningen af disse for borgerne. Demokratiet i DK og dets virkeform gennemgås. Du får en generel indføring i almen viden, som svarer til kravene til politiets optagelsesprøve.</li>
+<li><strong>Kommunikation og teambuilding</strong>: Du kommer til at stå i situationer, hvor du skal lave aktiviteter for de andre elever, og hvor I skal løse en opgave sammen.</li>
+<li><strong>Idræt</strong>: boldspil, fitness, svømning og vandaktiviter. Du kan vælge mellem forskellige muligheder efter din interesse. Der bliver i modsætning til de almindelige skolefag mulighed for afprøvning af mange forskellige idrætsgrene.</li>
+</ul>
 
-<p><strong>Torsdag:</strong> Morgensamling, idræt, dansk, samfundsfag, idræt, forberedelse, sang og musik.</p>
-<p><strong>Fredag:</strong> Morgensamling, idræt, matematik, idræt, forberedelse, socialt arrangement.</p>
-<p><strong>Lørdag:</strong> Udflugt</p>
-<p><strong>Søndag:</strong> Fri</p>
+<p>Du får mulighed for at få <strong>personling vejledning</strong> i forbindelse med valg af uddannelse, eller bare en snak om den uddannelse du skal i gang med eller er i gang med. Dette foregår dels i grupper med de øvrige elever, dels med en af skolens lærere.</p>
+<p>Du skulle gerne efter 3 uger på Vejle Idrætshøjskole have dannet dig et overblik over hvilke valg du kan træffe, og hvilke konsekvenser valgene har.</p>
+
+<p>Vejle Idrætshøjskole ligger cirka 4 km fra Vejle Centrum. Der er gode tog- og busforbindelser til Vejle. Højskolen ligger lige midt i skoven. Læs i øvrigt mere på vih.dk.</p>
 
 <h2>Pris</h2>
 <p>4500 kroner for alle tre uger. Er du ung med anden etnisk baggrund end dansk koster kurset kun kr. 500, fordi du er berettiget til støtte fra statens satspulje.</p>
