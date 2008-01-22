c0637969b2110a2026a3531970f70c6c6730cd01
Lars Olesen <lars@intraface.dk>
2008-01-22 19:42:12 +0000

    Added title, keywords and meta.

--- src/campsommer/controller/index.php
+++ src/campsommer/controller/index.php
@@ -3,6 +3,9 @@ class Campsommer_Controller_Index extends k_Controller
 {
     function GET()
     {
+        $this->document->title       = 'Sommercamp - 3 ugers højskole';
+        $this->document->keywords    = 'sommercamp, højskole';
+        $this->document->description = 'Sommercamp - 3 ugers højskole';
         return $this->render(dirname(__FILE__) . '/../templates/index.tpl.php');
     }
 }
